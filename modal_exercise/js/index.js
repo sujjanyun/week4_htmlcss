c34f1cef0aaf39a0f9412dc54d4db2826f0df960
Susan Yun <susanhkyun@gmail.com>
2021-02-08 14:32:24 -0600

    updating

---
+++ modal_exercise/js/index.js
@@ -0,0 +1,10 @@
+const showModal = document.querySelector('#showModal');
+const closeModal = document.querySelector('#closeModal');
+
+showModal.addEventListener('click', toggleModal());
+closeModal.addEventListener('click', toggleModal());
+
+function toggleModal() {
+    const modalOverlay = document.querySelector("#overlay");
+    modalOverlay.classList.toggle("visible")
+}
